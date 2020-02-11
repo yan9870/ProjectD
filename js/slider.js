5370f78561385f196c00f0ea95d34cfbb8a3d785
yan9870 <semeshchuk.yan@gmail.com>
2020-02-11 22:54:32 +0200

    форма заполнения почти доделана

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,46 @@
 $(document).ready(function() {
   $('.slider').slick({
-    // <i class="fas fa-chevron-left"></i>
     prevArrow: '<button type="button" class="slick-prev slick-arrow"><i class="fas fa-chevron-left"></i></button>',
-    nextArrow: '<button type="button" class="slick-next slick-arrow"><i class="fas fa-chevron-right"></i></button>'
+    nextArrow: '<button type="button" class="slick-next slick-arrow"><i class="fas fa-chevron-right"></i></button>',
+    slidesToShow: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        arrows: false
+      }
+    }]
+  });
+  $('.pick-pizza-slider').slick({
+    prevArrow: '<button type="button" class="slick-prev slick-arrow"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow"><i class="fas fa-chevron-right"></i></button>',
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true
+  });
+  $('.salads-slider').slick({
+    prevArrow: '<button type="button" class="slick-prev slick-arrow"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow"><i class="fas fa-chevron-right"></i></button>',
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true
   });
 });
+
+// $(".btn1").click(function() {
+//   $(".btn1").css("display", "none");
+// });
+// $(".btn2").click(function() {
+//   $(".btn2").css("display", "none");
+// });
+// $(".btn3").click(function() {
+//   $(".btn3").css("display", "none");
+// });
+// $(".btn4").click(function() {
+//   $(".btn4").css("display", "none");
+// });
+// $(".btn5").click(function() {
+//   $(".btn5").css("display", "none");
+// });
+// $(".btn6").click(function() {
+//   $(".btn6").css("display", "none");
+// });
